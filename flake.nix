e74ec56301e7052160fd1aee700663ec014c747f
Quentin <Quentin Inkling>
2021-02-11 17:57:08 +0300

    Update flake

--- flake.nix
+++ flake.nix
@@ -5,23 +5,36 @@
   };
 
   outputs = inputs@{ nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem
-      (system:
-        let
-          pkgs = nixpkgs.legacyPackages.${system};
-        in
-        {
-          devShell = pkgs.mkShell {
-            buildInputs = [
-              pkgs.gcc
-              pkgs.glibc
-              pkgs.rustc
-              pkgs.cargo
-              pkgs.pkg-config
-              pkgs.openssl.dev
-              pkgs.llvm_10
-            ];
-          };
-        }
-      );
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in rec {
+        devShell = pkgs.mkShell {
+          buildInputs = [
+            pkgs.gcc
+            pkgs.glibc
+            pkgs.rustc
+            pkgs.cargo
+            pkgs.llvm_10
+            pkgs.pkg-config
+            pkgs.openssl.dev
+          ];
+        };
+
+        defaultPackage = pkgs.rustPlatform.buildRustPackage rec {
+          pname = "versor";
+          version = "master";
+
+          src = ./.;
+
+          nativeBuildInputs = [ pkgs.pkg-config ];
+          buildInputs = [ pkgs.openssl ];
+
+          postInstall = ''
+            ln -s $out/bin/${pname} $out/bin/${pname}-${version}
+          '';
+
+          cargoSha256 = "sha256-SgAgLjlhBZLqUKV6mKRYeCJyjBZ2VErpP86labqLW+E=";
+        };
+      });
 }
